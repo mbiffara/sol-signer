ef65f93fcd368753336840469b6e164afadaed21
lauti fernandez <lautaro@pactta.app>
2023-07-11 16:37:57 -0300

    main refactor

--- src/controllers/send.js
+++ src/controllers/send.js
@@ -1,27 +1,30 @@
 import { transferFunds } from "../services/solWalletService.js";
+import { endRequest, catchRequest } from "../helpers/request.js";
 
 export const send = async (req, res) => {
-  if (req.body.address === undefined) {
-    res.status(400).send({ error: "address is required" });
-  }
-  if (req.body.private_key === undefined) {
-    res.status(400).send({ error: "private_key is required" });
-  }
-  if (req.body.destination === undefined) {
-    res.status(400).send({ error: "destination is required" });
-  }
-  if (req.body.amount === undefined) {
-    res.status(400).send({ error: "amount is required" });
-  }
-  if (req.body.symbol === undefined) {
-    res.status(400).send({ error: "token is required" });
-  }
-
   const fromAddress = req.body.address;
   const secretKey = req.body.private_key;
   const toAddress = req.body.destination;
   const amount = req.body.amount;
   const symbol = req.body.symbol;
 
-  res.send(transferFunds(fromAddress, toAddress, symbol, amount, secretKey));
+  const send = await transferFunds(
+    fromAddress,
+    toAddress,
+    symbol,
+    amount,
+    secretKey
+  );
+
+  if (send.length === 88) {
+    endRequest({ response: send, code: 200, res });
+  } else {
+    catchRequest({
+      err: {
+        code: 400,
+        message: `Error: ${send}`,
+      },
+      res: res,
+    });
+  }
 };
